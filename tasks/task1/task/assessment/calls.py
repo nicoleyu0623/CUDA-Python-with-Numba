fb3ae012166034e6c0727cd060eb2128e63837f1
hi sunyh <ethereal.fiora@gmail.com>
2020-06-15 12:29:42 +0800

    update calls.py

--- tasks/task1/task/assessment/calls.py
+++ tasks/task1/task/assessment/calls.py
@@ -2,13 +2,14 @@
 
 %%timeit
 # Feel free to modify the 3 function calls in this cell
-do_n = cuda.device_array(shape=(n,), dtype=np.float32)
-do_w = cuda.device_array(shape=(n,), dtype=np.float32)
-do_a = cuda.device_array(shape=(n,), dtype=np.float32)
-
 # normalized = normalize(greyscales)
 # weighted = weigh(normalized, weights)
 # SOLUTION = activate(weighted)
+from numba import cuda
+
+do_n = cuda.device_array(shape=(n,), dtype=np.float32)
+do_w = cuda.device_array(shape=(n,), dtype=np.float32)
+do_a = cuda.device_array(shape=(n,), dtype=np.float32)
 
 normalize(d_greyscales, out=do_n)
 weigh(do_n, d_weights, out=do_w)
