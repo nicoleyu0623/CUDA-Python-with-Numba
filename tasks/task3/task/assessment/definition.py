60375db8e403a10c79af830be89723c64bea5d70
hi sunyh <ethereal.fiora@gmail.com>
2020-06-16 11:30:13 +0800

    update definition.py

--- tasks/task3/task/assessment/definition.py
+++ tasks/task3/task/assessment/definition.py
@@ -3,35 +3,30 @@ import numpy as np
 from numba import cuda, types
 
 @cuda.jit
-def mm_shared(A, B, C):
-    x, y = cuda.grid(2)
+def mm_shared(a, b, c):
+    column, row = cuda.grid(2)
     sum = 0
     
     a_cache = cuda.shared.array(block_size, types.int32)
     b_cache = cuda.shared.array(block_size, types.int32)
 
-
     tx = cuda.threadIdx.x
     ty = cuda.threadIdx.y
-    bpg = M/N  # blocks per grid
 
-    if x >= C.shape[0] and y >= C.shape[1]:
-        # Quit if (x, y) is outside of valid C boundary
-        return
-
-    for i in range(bpg):
+    for i in range(a.shape[0]):
         # Preload data into shared memory
-        a_cache[tx, ty] = A[x, ty + i * N]
-        b_cache[tx, ty] = B[tx + i * N, y]
+        a_cache[tx][ty] = a[column][ty + i * N]
+        b_cache[tx][ty] = b[tx + i * N][ row]
 
         # Wait until all threads finish preloading
         cuda.syncthreads()
 
         # Computes partial product on the shared memory
-        for j in range(N):
-            sum += a_cache[tx, j] * b_cache[j, ty]
+        for j in range(a.shape[1]):
+            sum += a_cache[tx][j] * b_cache[j][ty]
 
         # Wait until all threads finish computing
         cuda.syncthreads()
 
-    C[x, y] = sum
+    c[column][row] = sum
+
