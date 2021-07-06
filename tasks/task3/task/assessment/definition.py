50cd649c6e57f2eddc1043792e9908092030f63f
Nicole Yu <x1n90623@gmail.com>
2021-07-06 22:13:08 +1000

    Update definition.py

--- tasks/task3/task/assessment/definition.py
+++ tasks/task3/task/assessment/definition.py
@@ -1,32 +1,25 @@
 # Use the 'File' menu above to 'Save' after pasting in your own mm_shared function definition.
+
 import numpy as np
 from numba import cuda, types
-
 @cuda.jit
 def mm_shared(a, b, c):
     column, row = cuda.grid(2)
     sum = 0
-    
+    stride_column,stride_row=cuda.gridsize(2)
+
     a_cache = cuda.shared.array(block_size, types.int32)
     b_cache = cuda.shared.array(block_size, types.int32)
+    tx=cuda.threadIdx.x
+    ty=cuda.threadIdx.y
+   
+    for i in range(row,a.shape[0],stride_row):
+        a_cache[ty,tx]=a[i,tx]   
+    cuda.syncthreads()
+    for j in range(column,b.shape[1],stride_column):
+        b_cache[ty,tx]=b[ty,j]
+    cuda.syncthreads()
+    for i in range(b_cache.shape[0]):
+        sum += a_cache[ty][i] * b_cache[i][tx]
 
-    tx = cuda.threadIdx.x
-    ty = cuda.threadIdx.y
-
-    for i in range(a.shape[0]):
-        # Preload data into shared memory
-        a_cache[tx][ty] = a[column][ty + i * N]
-        b_cache[tx][ty] = b[tx + i * N][ row]
-
-        # Wait until all threads finish preloading
-        cuda.syncthreads()
-
-        # Computes partial product on the shared memory
-        for j in range(a.shape[1]):
-            sum += a_cache[tx][j] * b_cache[j][ty]
-
-        # Wait until all threads finish computing
-        cuda.syncthreads()
-
-    c[column][row] = sum
-
+    c[row][column] = sum
